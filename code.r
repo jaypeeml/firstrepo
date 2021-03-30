83d2fd00debd37f61c3af0234c8df6be1cf0ffbf
jaypeeml <jaiprakash.prajapati@dgsl.in>
2021-03-30 21:38:15 +0530

    changes

--- code.r
+++ code.r
@@ -1,1 +1,20 @@
 ## https://www.geo.uzh.ch/microsite/reproducible_research/post/rr-rstudio-git/
+
+## Efficient way to install packages
+# Package names
+packages <- c("ggplot2", "readxl", "dplyr", "tidyr", "ggfortify", "DT", "reshape2", "knitr", "lubridate", "pwr", 
+              "psy", "car", "doBy", "imputeMissings", "RcmdrMisc", "questionr", "vcd", "multcomp",
+              "KappaGUI", "rcompanion", "FactoMineR", "factoextra", "corrplot", "ltm", "goeveg",
+              "corrplot", "FSA", "MASS", "scales", "nlme", "psych", "ordinal", "lmtest", "ggpubr", 
+              "dslabs", "stringr", "assist", "ggstatsplot", "forcats", "styler", "remedy", "snakecaser",
+              "addinslist", "esquisse", "here", "summarytools", "magrittr", "tidyverse", "funModeling", "pander", 
+              "cluster", "abind")
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
